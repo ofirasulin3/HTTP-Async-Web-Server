a96893af908c75ec97c5b7714e850161be0bcaba
ofirasulin3 <ofirasulin3@gmail.com>
2021-11-26 17:07:19 +0200

    post + delete requests support added. DB queries added but not tested yet

--- hw2.py
+++ hw2.py
@@ -1,9 +1,10 @@
-# import datetime
+import datetime
 import json
 import sqlite3
 import time
 import socket
 import hw2_utils
+import base64
 
 
 # def check_error(data, conn):
@@ -59,6 +60,10 @@ if __name__ == "__main__":
     SERVER_PORT = lines[0].split(" ")[2]
     admin_username = lines[2].split(" ")[3].split('\'')[1]
     admin_pass = lines[2].split(" ")[5].split('\'')[1]
+    pre_encoded = admin_username+":"+admin_pass
+    encoded = base64.b64encode(pre_encoded.encode())
+    # print("decoded admin credentials: ", base64.b64decode(encoded))
+    # print(b'YWRtaW46YWRtaW4=' == encoded)
 
     # conn_db = sqlite3.connect('users.db')
 
@@ -70,7 +75,7 @@ if __name__ == "__main__":
                 conn, addr = s.accept()
                 with conn:
                     timeout_start = time.time()
-                    while time.time() < timeout_start + timeout_param:
+                    while time.time() < timeout_start + float(timeout_param):
                         data = conn.recv(4096)
 
                         if not data:
@@ -116,12 +121,80 @@ if __name__ == "__main__":
                             # Authorization: Basic … …
                             # header K\r\n
                             # username=user1&password=1234"
-                            username_to_create = http_data[http_data.index("username=")+9:http_data.index("&")]
-                            userpass_to_create = http_data[http_data.index("password=")+9:]
+                            username_to_handle = http_data['body'][http_data['body'].index("username=") + 9:http_data['body'].index("&")]
+                            print("username_to_create: ", username_to_handle)
+                            userpass_to_handle = http_data['body'][http_data['body'].index("password=") + 9:]
+                            print("userpass_to_create: ", userpass_to_handle)
 
+                            auth_value = http_data['Authorization']
+                            if auth_value:  # TODO: there is no check like this. it sends KeyError and exits.
+                                # meaning there is credentials
+                                basic_str = "Basic "
+                                encoded = auth_value[len(basic_str):]
 
+                                decoded = base64.b64decode(encoded)
+                                admin_username_to_check = decoded.split(b':')[0].decode()
+                                # print("admin_username_to_check:", admin_username_to_check)
+                                admin_password_to_check = decoded.split(b':')[1].decode()
+                                # print("admin_password_to_check:", admin_password_to_check)
+                                if admin_username_to_check == admin_username \
+                                        and admin_password_to_check == admin_pass:
+                                    # create the user!
+                                    # add username_to_handle and userpass_to_handle to DB
+                                    print("create the user! add to the database")
+                                else:
+                                    # invalid admin credentials
+                                    print("invalid admin credentials! send the right error (401/403)")
 
+                            else:  # meaning there are no credentials
+                                # In case authorization field doesn't exist,
+                                # we will send a response with a form for authentication:
+                                response = str.encode(response_proto)
+                                response += b' '
+                                response_status = '401'
+                                response += str.encode(response_status)
+                                response += b' '
+                                response += str.encode("Unauthorized\r\n")
+                                response += b'\r\n'
 
+                                current_date = datetime.datetime.now()
+                                response_headers_date = current_date.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+                                # response_headers_content_type = "Content-Type: image/png"
+                                response_headers_content_len = "Content-Length: 0"
+                                response_headers = response_headers_date + "\r\n" + response_headers_content_len + "\r\n"
+                                response += str.encode(response_headers)
+                                response += str.encode("Connection: keep-alive\r\n")
+                                response += str.encode("WWW-Authenticate: Basic realm=\"HW2 realm\"\r\n")
+                                response += b'\r\n'  # to separate headers from body
+                                conn.sendall(response)
+
+                        if request_type == "DELETE":
+                            # DELETE /users/<username> HTTP/1.1\r\n
+                            # header
+                            # 1\r\n
+                            # Authorization: Basic … …
+                            # header # K\r\n
+                            auth_value = http_data['Authorization']
+                            if auth_value:  # TODO: there is no check like this. it sends KeyError and exits.
+                                print("meaning there is credentials")
+                                basic_str = "Basic "
+                                encoded = auth_value[len(basic_str):]
+
+                                decoded = base64.b64decode(encoded)
+                                admin_username_to_check = decoded.split(b':')[0].decode()
+                                admin_password_to_check = decoded.split(b':')[1].decode()
+                                if admin_username_to_check == admin_username \
+                                        and admin_password_to_check == admin_pass:
+                                    # check if the user exists on DB
+
+                                    # if user doesn't exists return Error 509 Conflict?
+
+                                    # if exists, delete the user!
+                                    # delete username_to_handle and userpass_to_handle from DB
+                                    print(" if exists, delete the user from database")
+                                else:
+                                    # invalid admin credentials
+                                    print("invalid admin credentials! send the right error (401/403)")
 
                         # 401 unauthorized
                         # 403 forbidden
--- hw2_utils.py
+++ hw2_utils.py
@@ -1,4 +1,78 @@
+import sqlite3
+from psycopg2 import sql
 
+
+def user_exists(username_to_check):
+    conn = None
+    try:
+        conn = sqlite3.connect('users.db')
+        query = sql.SQL("SELECT username, password"
+                        " FROM Users"
+                        " WHERE username = {var}").format(var=sql.Literal(username_to_check))
+        rows_effected, res = conn.execute(query)
+        conn.commit()
+    except Exception:
+        conn.close()
+        return False
+    conn.close()
+    if rows_effected <= 0:
+        return False
+    else:
+        return True
+
+
+def user_credentials_valid(username_to_check, password_to_check):
+    # if not user_exists(username_to_check):
+    #     return False
+    conn = None
+    try:
+        conn = sqlite3.connect('users.db')
+        query = sql.SQL("SELECT username, password"
+                        " FROM Users"
+                        " WHERE username = {var1} AND password = {var2}").format(var1=sql.Literal(username_to_check),
+                                          var2=sql.Literal(password_to_check))
+        rows_effected, res = conn.execute(query)
+        conn.commit()
+    except Exception:
+        conn.close()
+        return False
+    conn.close()
+    if rows_effected <= 0:
+        return False
+    else:
+        return True
+
+
+def user_insert(username_to_insert, password_to_insert):
+    conn = None
+    try:
+        conn = sqlite3.connect('users.db')
+        query = sql.SQL("INSERT INTO Users(username, password) VALUES({u_var}, {p_var});").format(u_var=sql.Literal(username_to_insert), p_var=sql.Literal(password_to_insert))
+        conn.execute(query)
+        conn.commit()
+    except Exception:
+        conn.close()
+        return False
+    conn.close()
+    return True
+
+def user_delete(username_to_delete):
+    # if not user_exists(username_to_check):
+    #     return False
+    conn = None
+    try:
+        conn = sqlite3.connect('users.db')
+        query = sql.SQL("DELETE FROM  Users WHERE username = {var1}").format(var1=sql.Literal(username_to_delete))
+        rows_effected, _ = conn.execute(query)
+        conn.commit()
+        if rows_effected == 0:
+            conn.close()
+            return False
+    except Exception:
+        conn.close()
+        return False
+    conn.close()
+    return True
 
 
 '''
@@ -14,10 +88,13 @@ def decode_http(http_data):
     http_dict = {}
     fields = http_data.decode('utf-8').split("\r\n")  # convert to string from bytes
     http_dict['Request'] = fields[0]
-    fields = fields[1:]
+    fields = fields[1:]  # -1 because we don't beed the body
     for field in fields:  # go over the fields
         if not field:
             continue
+        if field.find(':') == -1:
+            http_dict['body'] = field
+            break
         key, value = field.split(':', 1)  # split each line by http field name and value
         http_dict[key] = value
     return http_dict
